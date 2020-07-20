73a6df54fc7e5a3c2745acdf7fcefef52a76e82c
xiaoxuewen <xuewen.xiao@Fastrain.com>
2020-07-20 19:32:34 +0800

    change login function

--- pub/js/login.js
+++ pub/js/login.js
@@ -1,45 +1,43 @@
-$(function() {
-    $("#login_btn").click(function() {
-        var $userId = $("#userId").val();
-        var $pwd = $("#pwd").val();
-        if ($userId == "") {
-            $("#info-user").text("Account number is empty").css({
-                "font-weight": "bold",
-                "color": "red"
-            });
-            event.preventDefault();
-        } else if ($pwd == "") {
-            $("#info-pass").text("The password is empty").css({
-                "font-weight": "bold",
-                "color": "red"
-            });
-            event.preventDefault();
-        } else {
+$("#login_btn").click(function() {
+    var $userId = $("#userId");
+    var $pwd = $("#pwd");
+    if ($userId.val() == "") {
+        $userId.next().text("Account number is empty").css({
+            "font-weight": "bold",
+            "color": "red"
+        });
+        event.preventDefault();
+    } else if ($pwd.val() == "") {
+        $pwd.next().text("The password is empty").css({
+            "font-weight": "bold",
+            "color": "red"
+        });
+        event.preventDefault();
+        // } else {
 
-            $.ajax({
-                url: "/action/login",
-                type: "post",
-                data: {
-                    username: $userId.val(),
-                    password: $pwd.val()
-                },
-                cache: false,
-                success: function(res) {
-                    if (res.result.login == "0") {
-                        $userId.next().text("Wrong user name or password").css({
-                            "font-weight": "bold",
-                            "color": "red"
-                        });
-                        $pwd.next().text("Wrong user name or password").css({
-                            "font-weight": "bold",
-                            "color": "red"
-                        });
-                    } else {
-                        alert("Login successful");
-                    }
+        //     $.ajax({
+        //         url: "/action/login",
+        //         type: "post",
+        //         data: {
+        //             username: $userId.val(),
+        //             password: $pwd.val()
+        //         },
+        //         success: function(res) {
+        //             if (res.result.login == "0") {
+        //                 $userId.next().text("Wrong user name or password").css({
+        //                     "font-weight": "bold",
+        //                     "color": "red"
+        //                 });
+        //                 $pwd.next().text("Wrong user name or password").css({
+        //                     "font-weight": "bold",
+        //                     "color": "red"
+        //                 });
+        //             } else {
 
-                }
-            });
-        }
-    });
-})
+        //                 Top.Location.href = "index.html";
+        //             }
+
+        //         }
+        //     });
+    }
+});
