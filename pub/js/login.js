8a0515e367f5da806f5074f490ddc0adf5b5910e
xiaoxuewen <xuewen.xiao@Fastrain.com>
2020-07-22 14:27:19 +0800

    modify index.html and login.js

--- pub/js/login.js
+++ pub/js/login.js
@@ -18,12 +18,13 @@ $("#login_btn").click(function() {
         $.ajax({
             url: "/action/login",
             type: "post",
-            data: {
-                username: $userId.val(),
-                password: $pwd.val()
-            },
-            // data = JSON.stringify(data),
-            // data=serialize(data),
+            data: $('#loginInfo').serialize(),
+            dataType: "json",
+            contentType: "application/x-www-form-urlencoded;charset=utf-8",
+            //         data: {
+            //             username: $userId.val(),
+            //             password: $pwd.val()
+            //         },
             success: function(res) {
                 if (res.result.login == "0") {
                     $userId.next().text("Wrong user name or password").css({
@@ -35,11 +36,59 @@ $("#login_btn").click(function() {
                         "color": "red"
                     });
                 } else {
-
-                    Top.Location.href = "index.html";
+                    //	window.location.href = "/index.html";
+                    top.location.href = "/index.html";
                 }
 
             }
         });
     }
 });
+
+
+
+// $("#login_btn").click(function() {
+//     var $userId = $("#userId");
+//     var $pwd = $("#pwd");
+//     if ($userId.val() == "") {
+//         $userId.next().text("Account number is empty").css({
+//             "font-weight": "bold",
+//             "color": "red"
+//         });
+//         event.preventDefault();
+//     } else if ($pwd.val() == "") {
+//         $pwd.next().text("The password is empty").css({
+//             "font-weight": "bold",
+//             "color": "red"
+//         });
+//         event.preventDefault();
+//     } else {
+
+//         $.ajax({
+//             url: "/action/login",
+//             type: "post",
+//             data: {
+//                 username: $userId.val(),
+//                 password: $pwd.val()
+//             },
+//             // data = JSON.stringify(data),
+//             // data=serialize(data),
+//             success: function(res) {
+//                 if (res.result.login == "0") {
+//                     $userId.next().text("Wrong user name or password").css({
+//                         "font-weight": "bold",
+//                         "color": "red"
+//                     });
+//                     $pwd.next().text("Wrong user name or password").css({
+//                         "font-weight": "bold",
+//                         "color": "red"
+//                     });
+//                 } else {
+
+//                     Top.Location.href = "index.html";
+//                 }
+
+//             }
+//         });
+//     }
+// });
