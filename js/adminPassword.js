6d4e5e2ef9a1383be0056f02b2bd43249e3fa94a
xiaoxuewen <xuewen.xiao@Fastrain.com>
2020-07-22 10:35:10 +0800

    change adminPassword.html,css/bootstrap.min.css,css/bootstrap/3.3.7/bootstrap.min.css,flashops.html,index.html,js/adminPassword.js,js/jquery/2.1.1/jquery.min.js,pub/css/bootstrap.min.css,pub/login.html

--- js/adminPassword.js
+++ js/adminPassword.js
@@ -1,34 +1,65 @@
 $(function() {
-    $('#saveBtn').click(function() {
-        var data = {
-            "jsonrpc": "2.0",
-            "method": "change_password",
-            "params": {
-                "oldPwd": $("#pwd").val(),
-                "newPwd": $("#newPwd").val()
-            },
-            "id": "9.1"
-        };
+    $('saveBtn').one('click', function() {
+            var data = {
+                "jsonrpc": "2.0",
+                "method": "change_password",
+                "params": {
+                    "oldPwd": $("#pwd").val(),
+                    "newPwd": $("#newPwd").val()
+                },
+                "id": "9.1"
+            };
 
-        data = JSON.stringify(data);
-        $.ajax({
-            type: "post",
-            url: "/action/password",
-            data: data,
-            dataType: "json",
-            contentType: "application/json;charset=utf-8",
-            success: function(data) {
-                if (data.status == "success") {
-                    console.log(data.msg);
-                } else {
-                    console.log("出现错误：" + data.msg);
+            data = JSON.stringify(data);
+            $.ajax({
+                type: "post",
+                url: "/action/password",
+                data: data,
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                success: function(data) {
+                    if (data.status == "success") {
+                        console.log(data.msg);
+                    } else {
+                        console.log("An error occurred：" + data.msg);
+                    }
+                },
+                error: function(jqXHR) {
+                    alert("An error occurred：" + jqXHR.status);
+
                 }
-            },
-            error: function(jqXHR) {
-                alert("发生错误：" + jqXHR.status);
+            });
+        })
+        // $('#saveBtn').click(function() {
+        //     var data = {
+        //         "jsonrpc": "2.0",
+        //         "method": "change_password",
+        //         "params": {
+        //             "oldPwd": $("#pwd").val(),
+        //             "newPwd": $("#newPwd").val()
+        //         },
+        //         "id": "9.1"
+        //     };
 
-            }
-        });
+    //     data = JSON.stringify(data);
+    //     $.ajax({
+    //         type: "post",
+    //         url: "/action/password",
+    //         data: data,
+    //         dataType: "json",
+    //         contentType: "application/json;charset=utf-8",
+    //         success: function(data) {
+    //             if (data.status == "success") {
+    //                 console.log(data.msg);
+    //             } else {
+    //                 console.log("出现错误：" + data.msg);
+    //             }
+    //         },
+    //         error: function(jqXHR) {
+    //             alert("发生错误：" + jqXHR.status);
 
-    });
+    //         }
+    //     });
+
+    // });
 })
