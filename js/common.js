4c27ea70a82de4f3071f7b3b2439147fac0926ca
yisizheng <sizheng.yi@fastrain.com>
2020-12-01 09:35:44 +0800

    modify restart

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
-//var req = "/proxy";
-var req = "";
+var req = "/proxy";
+var restart0 = true;
+//var req = "";
 //获取url中的参数
 function GetUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
