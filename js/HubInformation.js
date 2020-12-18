6a03cec0753a895ad3d14ea7bb1b4581e68a704f
yisizheng <sizheng.yi@fastrain.com>
2020-12-18 10:05:48 +0800

    upgrade

--- js/HubInformation.js
+++ js/HubInformation.js
@@ -20,18 +20,18 @@ $(function() {
         }
     }
 
-    // function getDuration(my_time) {
-    //     var days = my_time / 1000 / 60 / 60 / 24;
-    //     var daysRound = Math.floor(days);
-    //     var hours = my_time / 1000 / 60 / 60 - (24 * daysRound);
-    //     var hoursRound = Math.floor(hours);
-    //     var minutes = my_time / 1000 / 60 - (24 * 60 * daysRound) - (60 * hoursRound);
-    //     var minutesRound = Math.floor(minutes);
-    //     var seconds = my_time / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound);
-    //     console.log('转换时间:', daysRound + '天', hoursRound + '时', minutesRound + '分', seconds + '秒');
-    //     var time = hoursRound + ':' + minutesRound + ':' + seconds
-    //     return time;
-    // }
+    $("#btnSave").click(function() {
+        var domain = window.location.host;
+        // self.location.href = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/action/down";
+        //console.log(self.location.href + "/action/down");
+        //console.log(('https:' == document.location.protocol ? 'https://' : 'http://') + domain);
+        //console.log(('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/action/down");
+        window.location.href = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/action/down";
+        //console.log(self.location.href = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/action/down")
+        //window.open("http://198.18.248.1/action/down", "_self");
+        //console.log(window.open("/action/down", "_self"))
+    });
+
 
     function wd(m) {
         var h = 5 / 9.0 * (m - 32) + 273.15; //热力学温度T与人们惯用的摄氏温度t的关系是：T（K）=273.15+t(℃)。
@@ -57,35 +57,30 @@ $(function() {
         dataType: "json",
         contentType: "application/json;charset=utf-8",
         success: function(res) {
-
-            $("#model").html(res.result.Model);
-            $("#iv").html(res.result.ipq_version);
-            $("#nv").html(res.result.nxp_version);
-            $("#stm8").html(res.result.stm8_version);
-            $("#sn").html(res.result.serial_num);
-            $("#iMeiInfo").html(res.result.IMEI);
-            $("#voltage").html(res.result.Voltage + " V");
-            $("#cd").html(res.result.Current_draw + " A");
-            var Temperature = (res.result.Temperature - 272.15).toFixed(2);
-            $("#temperature").html(Temperature + " ℃");
-            var hour = (res.result.Operating_hours / 3600).toFixed(2)
-            $("#oh").html(hour + " hours");
-            $("#macAddress1").html(res.result.imax_mac);
-            $("#macAddress2").html(res.result.ipq_ether_mac);
-            $("#aPma").html(res.result.ipq_wifi1_mac);
-            $("#ipqAddress").html(res.result.ipq_ipaddr);
-            $("#ipqAddress").attr("href", "http://" + res.result.ipq_ipaddr);
-            var domain = window.location.host;
-            $("#btnSave").click(function() {
-                // self.location.href = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/action/down";
-                //console.log(self.location.href + "/action/down");
-                //console.log(('https:' == document.location.protocol ? 'https://' : 'http://') + domain);
-                //console.log(('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/action/down");
-                window.location.href = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/action/down";
-                //console.log(self.location.href = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/action/down")
-                //window.open("http://198.18.248.1/action/down", "_self");
-                //console.log(window.open("/action/down", "_self"))
-            });
+            if (res.result) {
+                $("#model").html(res.result.Model);
+                $("#iv").html(res.result.ipq_version);
+                $("#nv").html(res.result.nxp_version);
+                $("#stm8").html(res.result.stm8_version);
+                $("#sn").html(res.result.serial_num);
+                $("#iMeiInfo").html(res.result.IMEI);
+                $("#voltage").html(res.result.Voltage + " V");
+                $("#cd").html(res.result.Current_draw + " A");
+                var Temperature = (res.result.Temperature - 272.15).toFixed(2);
+                $("#temperature").html(Temperature + " ℃");
+                var hour = (res.result.Operating_hours / 3600).toFixed(2)
+                $("#oh").html(hour + " hours");
+                $("#macAddress1").html(res.result.imax_mac);
+                $("#macAddress2").html(res.result.ipq_ether_mac);
+                $("#aPma").html(res.result.ipq_wifi1_mac);
+                $("#ipqAddress").html(res.result.ipq_ipaddr);
+                $("#ipqAddress").attr("href", "http://" + res.result.ipq_ipaddr);
+            } else if (res.error) {
+                layui.use(['form', 'layer'], function() {
+                    var layer = layui.layer;
+                    layer.msg("An error occurred：" + res.error.message);
+                })
+            }
         },
         error: function(jqXHR) {
             var tip = '<div style="padding: 20px;text-align: center;word-wrap:break-word;">' + JSON.stringify(jqXHR) + '</div>';
--- js/connectedDevices.js
+++ js/connectedDevices.js
@@ -10,33 +10,40 @@ $(function() {
         dataType: "json",
         contentType: "application/json;charset=utf-8",
         success: function(res) {
-            var json = res.result.dev_list;
-            var main = "";
+            if (res.result && res.result.dev_list) {
+                var json = res.result.dev_list;
+                var main = "";
 
-            //main += '<table class="table table-bordered table-responsive table-condensed table-hover">';
-            // main += '<thead><tr><th class="text-center Device-name active">Device</th><th class="text-center Device-name active">Device name</th><th class="text-center Device-name active">MAC address</th><th class="text-center Device-name active">IP address</th><th class="text-center Device-name active">Connection type</th></tr></thead>';
-            for (var item in json) {
-                main += '<tr class="text-center"><td class="align"><img class="img_a" src=""></td><td class="align"><span id="DeviceName"></span>' + json[item].DName + '</td><td class="align"><span id="MACAddress"></span>' + json[item].MACAddress + '</td><td class="align"><span id="IPAddress"></span>' + json[item].IPAddress + '</td><td class="align"><span id="ConnectMode" class="ConnectMode"></span>' + json[item].DType + '</td></tr>';
-            }
-            //main += '</table>'
-            $('#main').html(main);
+                //main += '<table class="table table-bordered table-responsive table-condensed table-hover">';
+                // main += '<thead><tr><th class="text-center Device-name active">Device</th><th class="text-center Device-name active">Device name</th><th class="text-center Device-name active">MAC address</th><th class="text-center Device-name active">IP address</th><th class="text-center Device-name active">Connection type</th></tr></thead>';
+                for (var item in json) {
+                    main += '<tr class="text-center"><td class="align"><img class="img_a" src=""></td><td class="align"><span id="DeviceName"></span>' + json[item].DName + '</td><td class="align"><span id="MACAddress"></span>' + json[item].MACAddress + '</td><td class="align"><span id="IPAddress"></span>' + json[item].IPAddress + '</td><td class="align"><span id="ConnectMode" class="ConnectMode"></span>' + json[item].DType + '</td></tr>';
+                }
+                //main += '</table>'
+                $('#main').html(main);
 
-            var arr = ['Ethernet', 'Wireless'];
-            var arrImg = ['Group 2902.png', 'Group 2901.png'];
-            for (var i = 0; i < json.length; i++) {
-                if (json[i].DType == 0) {
-                    $('.ConnectMode').eq(i).text(arr[0]);
-                    $(".img_a").eq(i).attr("src", 'images/' + arrImg[0]);
-                } else if (json[i].DType == 1) {
-                    $('.ConnectMode').eq(i).text(arr[1]);
-                    $(".img_a").eq(i).attr("src", 'images/' + arrImg[1]);
-                };
+                var arr = ['Ethernet', 'Wireless'];
+                var arrImg = ['Group 2902.png', 'Group 2901.png'];
+                for (var i = 0; i < json.length; i++) {
+                    if (json[i].DType == 0) {
+                        $('.ConnectMode').eq(i).text(arr[0]);
+                        $(".img_a").eq(i).attr("src", 'images/' + arrImg[0]);
+                    } else if (json[i].DType == 1) {
+                        $('.ConnectMode').eq(i).text(arr[1]);
+                        $(".img_a").eq(i).attr("src", 'images/' + arrImg[1]);
+                    };
+                }
+            } else if (res.error) {
+                layui.use(['form', 'layer'], function() {
+                    var layer = layui.layer;
+                    layer.msg("An error occurred：" + res.error.message);
+                })
             }
+
         },
-
         error: function(jqXHR) {
-            alert("发生错误：" + jqXHR.status);
-
+            var tip = '<div style="padding: 20px;text-align: center;word-wrap:break-word;">' + JSON.stringify(jqXHR) + '</div>';
+            promptMessage("Error message", tip);
         }
     })
 
