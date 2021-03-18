3a502729254a59c3ccbd4b04cea1738564f73157
yisizheng <sizheng.yi@fastrain.com>
2021-03-18 17:28:34 +0800

    upgrade

--- js/upload.js
+++ js/upload.js
@@ -1,132 +1,140 @@
-﻿$(function() {
+﻿var frequency = 0;
+ $(function() {
+        getVersion();
 
-    layui.use(['layer', 'element', 'form', 'upload'], function() {
+        $("#fileUpload").change(function () {         
+                var files=document.getElementById("fileUpload").files;
+                console.log(files)
+                 toUploading(files);
+        })
+
+            var box = document.querySelector('#upload');
+
+            //只要鼠标拖拽悬停在该区域就会触发
+            box.addEventListener('dragover', function(e) {
+                e.preventDefault(); //注意，如果dragover不阻止默认事件，drop事件就不会触发
+                console.log('dragover');
+            }, false);
+            //鼠标拖拽释放
+            box.addEventListener('drop', function(e) {
+                e.preventDefault(); //浏览器默认会打开该文件，因此停掉该默认事件
+                //选中的文件
+                var files = e.dataTransfer.files;
+                toUploading(files);
+                
+            }, false);
+
+        $("#upgrade").on("click", function() {
+            var has = $(this).hasClass("disable-btn");
+            if (has) {
+                return;
+            } else {
+                toUpgrade();
+            }
+        })
+  
+    })
+function toUploading(files){
+         layui.use(['layer', 'element', 'form', 'upload'], function() {
             var layer = layui.layer,
                 element = layui.element,
-                form = layui.form,
-                upload = layui.upload;
+                form = layui.form;
+        var formData = new FormData();
 
-            //创建监听函数
-            var xhrOnProgress = function(fun) {
-                xhrOnProgress.onprogress = fun; //绑定监听
-                //使用闭包实现监听绑
-                return function() {
-                    //通过$.ajaxSettings.xhr();获得XMLHttpRequest对象
-                    var xhr = $.ajaxSettings.xhr();
-                    //判断监听函数是否为函数
-                    if (typeof xhrOnProgress.onprogress !== 'function')
-                        return xhr;
-                    //如果有监听函数并且xhr对象支持绑定时就把监听函数绑定上去
-                    if (xhrOnProgress.onprogress && xhr.upload) {
-                        xhr.upload.onprogress = xhrOnProgress.onprogress;
-                    }
-                    return xhr;
+                Array.prototype.slice.call(files).forEach(function(file) {
+                    formData.append('file', file);
+                });
+                formData.forEach(function (value, key) {
+                if (value instanceof File) {
+                       formData.set(key, value, value.name.replace(/ /g, ''))
+                   }
+                })
+                formData.append("mode", "0");
+                console.log(formData);
+                var xhr = new XMLHttpRequest();
+                xhr.upload.onerror = function(error) {
+                    console.log(error)
+                    layer.msg("Upload fail！");
                 }
-            }
-
-            var uploadFile = upload.render({
-                elem: '#upload', //绑定元素
-                //url: 'api/upload', //上传接口
-                url: '/action/upload', //上传接口
-                exts: 'tar', //限定上传类型
-                //accept: images,//指定允许上传时校验的文件类型 images（图片）、file（所有文件）、video（视频）、audio（音频）
-                accept: File,
-                //acceptMime: 'image/jpg, image/png, image/jpeg', //只筛选上述类型图片
-                //number： '0',//0为不限制上传数量
-                xhr: xhrOnProgress,
-                data: { mode: 0 }, //可选项 额外的参数，如：{id: 123, abc: 'xxx'}
-                //multiple: true,// 开启多文件上传
-                drag: false, //是否允许拖拽上传
-                size: 0, //为0为不限制大小
-                //size: 1024 * 3, //为0为不限制大小
-                //监听xhr进度，并返回给进度条
-                progress: function(value, elem) { //上传进度回调 value进度值  
+                xhr.upload.onload = function() {
+                    console.log('上传成功');
+                }
+                $("#upload_progress").removeClass("layui-hide");
+                xhr.upload.onprogress = function(e) {
+                    var value = Math.floor(100 * e.loaded / e.total);
                     element.progress('upload_progress', value + '%') //设置页面进度条
                     $("#upgrade").prop("disabled", true);
-                },
-                before: function(obj) {
-                    //开始上传时候让进度条去除隐藏状态
-                    $("#upload_progress").removeClass("layui-hide");
-                    //或者设置loading
-                    //layer.load(1); //去除方法为 layer.close('loading'); 或者 layer.closeAll('loading');
-                },
-                //auto: false, //选择文件后不自动上传 默认值为true
-                //bindAction: '#testListAction', //指向一个按钮触发上传
-                //选择文件的回调 在文件被选择后触发，该回调会在 before 回调之前。一般用于非自动上传（即 auto: false ）的场景 预览图片等其他操作
-                /*choose: function(obj){
-   				//将每次选择的文件追加到文件队列
-    			var files = obj.pushFile();
-				//预读本地文件，如果是多文件，则会遍历。(不支持ie8/9)
-    			obj.preview(function(index, file, result){
-			  	console.log(index); //得到文件索引
-			 	console.log(file); //得到文件对象
-			  	console.log(result); //得到文件base64编码，比如图片
-      			//obj.resetFile(index, file, '123.jpg'); //重命名文件名
-      			//这里还可以做一些 append 文件列表 DOM 的操作
-      			//obj.upload(index, file); //对上传失败的单个文件重新上传，一般在某个事件中使用
-      			//delete files[index]; //删除列表中对应的文件，一般在某个事件中使用
-				 obj.preview(function(index, file, result){
-					 //对上传失败的单个文件重新上传，一般在某个事件中使用
-					 //obj.upload(index, file);
-				});
-    		});
-			*/
+                }
+                xhr.open('post', '/action/upload', true);
+                xhr.send(formData);
+                xhr.onreadystatechange = state_Change;
 
-                done: function(res, index, upload) { //在多文件上传中 每次成功将被调用一次
-                    /*  你不一定非得按照上述格式返回，只要是合法的 JSON 字符即可。其响应信息会转化成JS对象传递给 done 回调。
-				{	如果上传后，出现文件下载框（一般为ie下），那么你需要在服务端对response的header设置 Content-Type: text/html
-  					"code": 0,
-  					"msg": "",
- 					"data": {
-    					"src": "http://google.com"
- 				 	}
-				}       
-				*/
-                    //上传完毕回调
-                    //假设code=0代表上传成功
-                    layer.close(layer.index);
-
-                    if (res.code == 0) { //此处只用于单文件图片的上传，若为多图片多文件请做其他处理
-                        layer.msg("Upload successful！");
-                        $("#upgrade").removeClass("disable-btn")
-                            // setTimeout(function() {
-                            //     window.location.href = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + "/flashops.html";
-                            // }, 1000);
-                        $("#version").html(res.id);
-                        console.log(res)
+                function state_Change() {
+                    console.log("xhr",xhr)
+                    if (xhr.readyState == 4) { // 4 = "loaded"
+                        if (xhr.status == 200) { // 200 = OK
+                            var resObj = JSON.parse(xhr.response)
+                            if (resObj.code == 0) {
+                                layer.msg("Upload successful！");
+                                $("#upgrade").removeClass("disable-btn");
+                                console.log(resObj)
+                                $("#version").html(resObj.id);
+                            } else {
+                                layer.msg("Upload fail:" + msg);
+                            }
+                        } else {
+                            layer.msg("Upload fail！");
+                        }
                     }
-                },
-                error: function(index, upload) {
-                    //请求异常回调
-                    layer.close(layer.index);
-                    layer.confirm("Upload failed, do you want to upload again?", {
-                        btn: ['OK', 'Cancel'],
-                        icon: 3,
-                        title: "Tips"
-                    }, function() {
-                        //关闭询问框
-                        layer.closeAll();
-                        //重新调用上传方法
-                        uploadFile.upload();
+                }
+         })
+    }
+    function getVersion() {
+        var data = {
+            "jsonrpc": "2.0",
+            "method": "GetHubInfo",
+            "params": {
+
+            },
+            "id": "9.1"
+        };
+
+        data = JSON.stringify(data);
+        $.ajax({
+            type: "post",
+            url: "/action/action",
+            data: data,
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            success: function(res) {
+                if (res.result) {
+                    $("#version").html(res.result.TotalVersion);
+                    $("#Platform_version").html(res.result.PlatformVersion ? res.result.PlatformVersion :
+                        "--");
+                    $("#Product_version").html(res.result.BundleVersion);
+                } else if (res.error) {
+                    layui.use(['form', 'layer'], function() {
+                        var layer = layui.layer;
+                        layer.msg("An error occurred：" + res.error.message);
                     })
                 }
-            });
-
-
-        }) //layui
-
-    $("#upgrade").on("click", function() {
-        var has = $(this).hasClass("disable-btn");
-        if (has) {
-            return;
-        } else {
-            toUpgrade();
-        }
-    })
-})
-
+            },
+            error: function(jqXHR) {
+                console.log(JSON.stringify(jqXHR))
+            frequency++;
+            if (frequency < 3) {
+                setTimeout(() => {
+                    getVersion();
+                }, 5000);
+            } else {
+                frequency = 0;
+                var tip = '<div style="padding: 20px;text-align: center;word-wrap:break-word;">Abnormal communication!</div>';
+                promptMessage("Error message", tip);
+            }
+            }
+        });
+    }
 function toUpgrade() {
-
     $.ajax({
         url: "/action/upload",
         type: "post",
